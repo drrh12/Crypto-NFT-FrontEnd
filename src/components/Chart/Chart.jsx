124c9c92b83c4da39803fbd80d15455d97bbef55
flavio <flavioandrade0201@gmail.com>
2021-09-14 19:54:11 +0300

    Coin files added

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,17 +1,32 @@
 import React, { useState, useEffect } from "react";
+import { Search } from "./ChartElements";
 
-const axios = require("axios");
-
-// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
+import axios from "axios";
 
 function Chart() {
+  const [coins, setCoins] = useState([]);
+
   useEffect(() => {
-    axios.get(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-    );
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+      })
+      .catch((error) => console.log(error));
   }, []);
 
-  return <div></div>;
+  return (
+    <>
+      <Search>
+        <h1>Search a crypto currency</h1>
+        <form action="">
+          <input type="text" placeholder="Search" />
+        </form>
+      </Search>
+    </>
+  );
 }
 
 export default Chart;
--- src/components/Crypto/Crypto.jsx
+++ src/components/Crypto/Crypto.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Chart from "../Chart/Chart";
 
 function Crypto() {
   return (
     <div>
-      <h1>Crypto</h1>
+      <Chart></Chart>
     </div>
   );
 }
