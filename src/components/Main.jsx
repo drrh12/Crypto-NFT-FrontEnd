7232c9c05fa1b78acf57fba125c96aa0f00bb88b
flavio <flavioandrade0201@gmail.com>
2021-09-14 19:30:08 +0300

    Chart files added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-
 import { Menu, MainComponent } from "./MainElements";
-
 import MarketPlace from "./MarketPlace/MarketPlace";
 import Crypto from "./Crypto/Crypto";
-
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 function Main() {
   return (
