8f68e1dc5ee8dec4c2914feeedc5a518d6f5ed70
flavio <flavioandrade0201@gmail.com>
2021-09-14 19:59:51 +0300

    Coin.jsx component and CoinElements.jsx styled components coded and created

--- src/components/Coin/Coin.jsx
+++ src/components/Coin/Coin.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
-function Coin() {
-  return <div></div>;
+import {
+  CoinContainer,
+  CoinRow,
+  Coins,
+  CoinData,
+  CoinPrice,
+  CoinVolume,
+} from "./CoinElements";
+
+function Coin({ name, image, symbol, price, volume }) {
+  return (
+    <>
+      <CoinContainer>
+        <CoinRow>
+          <Coins>
+            <img src={image} alt="images-crypto" />
+            <h1>{name}</h1>
+            <p>{symbol}</p>
+          </Coins>
+        </CoinRow>
+        <CoinData>
+          <CoinPrice>${price}</CoinPrice>
+          <CoinVolume>${volume.toLocaleString()}</CoinVolume>
+        </CoinData>
+      </CoinContainer>
+    </>
+  );
 }
 
 export default Coin;
---
+++ src/components/Coin/CoinElements.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const CoinContainer = styled.div``;
+export const CoinRow = styled.div``;
+export const Coins = styled.div``;
+export const CoinData = styled.div``;
+export const CoinPrice = styled.div``;
+export const CoinVolume = styled.div``;
