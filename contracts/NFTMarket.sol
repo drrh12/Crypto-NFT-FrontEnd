4b4dcc1ad6458ddca32f1ac47416318d9f6ee005
flavio <flavioandrade0201@gmail.com>
2021-09-15 20:05:41 +0300

    NFTMarket contract

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -1,3 +1,15 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract NFTMarket is ReentrancyGuard {
+    using Counters for Counters.Counter;
+    Counters.Counter private _ItemsIds;
+    Counters.Counter private _itemSold;
+
+    address payable owner;
+}
