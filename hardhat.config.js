9632a723b504c8657d265e0159dcadd60cd0d750
flavio <flavioandrade0201@gmail.com>
2021-09-15 19:34:09 +0300

    Polygon network configured

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,27 @@
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+const fs = require("fs");
+const privateKey = fs.readFileSync(".secret").toString();
+//Move to a .ENV later
+const projectId = "1147d6f66b4d422e928010748b7f8f6c";
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
+  //Network configurations
+  networks: {
+    //local
+    hardhat: {
+      chainId: 1337,
+    },
+    //polygon mumbai
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
+    },
+    //polygon mainnet
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
+    },
+  },
   solidity: "0.8.4",
 };
