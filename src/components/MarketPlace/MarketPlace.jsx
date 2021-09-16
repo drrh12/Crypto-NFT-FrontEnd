1e5f7a4efe976ef43742ae3ae1e535d6ee114bc1
flavio <flavioandrade0201@gmail.com>
2021-09-16 18:59:20 +0300

    MarketPlace visualization in React

--- src/components/MarketPlace/MarketPlace.jsx
+++ src/components/MarketPlace/MarketPlace.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import { Menu } from "./MarketPlaceElements";
 
 function MarketPlace() {
   return (
     <div>
-      <h1>Market Place</h1>
+      <Menu>
+        <Link exact to="/crypto">
+          <h4>Sell digital assets</h4>
+        </Link>
+        <Link exact to="/marketplace">
+          <h4>My assets</h4>
+        </Link>
+        <Link exact to="">
+          <h4>Creator dashboard</h4>
+        </Link>
+      </Menu>
+      <Switch>
+        <Route path="/crypto" component={Crypto} />
+        <Route path="/marketplace" component={MarketPlace} />
+      </Switch>
     </div>
   );
 }
