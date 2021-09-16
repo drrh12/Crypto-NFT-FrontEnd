245481164e7d7d1138471a2cc9acab051559f8f9
flavio <flavioandrade0201@gmail.com>
2021-09-16 19:22:11 +0300

    frontend function for loading the nfts

--- src/components/MarketPlace/MarketPlace.jsx
+++ src/components/MarketPlace/MarketPlace.jsx
@@ -1,11 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Menu, MainContainer } from "./MarketPlaceElements";
+import axios from "axios";
+import Web3Modal from "web3modal";
 
-import { Menu } from "./MarketPlaceElements";
+import { nftaddress, nftmarketaddress } from "./config";
+
+import NFT from "../../../artifacts/contracts/NFT.sol/NFT.json";
+import Market from "../../../artifacts/contracts/NFTMarket.sol/NFTMarket.json";
+import { ethers } from "ethers";
 
 function MarketPlace() {
+  const [nfts, setNfts] = useState([]);
+  const [loadingState, setLoadingState] = useState("not-loaded");
+
+  useEffect(() => {
+    loadNfts();
+  }, []);
+
+  async function loadNfts() {
+    const provider = new ethers.providers.JsonRpcProvider();
+    const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider);
+    const marketContract = new ethers.Contract(
+      nftmarketaddress,
+      Market.abi,
+      provider
+    );
+    const data = await marketContract.fetchMarketItems();
+
+    const items = await Promise.all(
+      data.map(async (i) => {
+        const tokenUri = await tokenContract.tokenUri(i.tokenId);
+        const meta = await axios.get(tokenUri);
+        let price = ethers.utils.formatUnits(i.price.toString(), "ether");
+
+        let item = {
+          price,
+          tokenId: i.tokenId.toNumber(),
+          seller: i.seller,
+          owner: i.owner,
+          image: meta.data.name,
+          name: meta.data.name,
+          description: meta.data.description,
+        };
+        return item;
+      })
+    );
+
+    setNfts(items);
+    setLoadingState("loaded");
+  }
+
+  if (loadingState === "loaded" && !nfts.length)
+    return <h4>No items in marketplace</h4>;
+
   return (
-    <div>
+    <MainContainer>
       <Menu>
         <Link exact to="/crypto">
           <h4>Sell digital assets</h4>
@@ -18,10 +68,10 @@ function MarketPlace() {
         </Link>
       </Menu>
       <Switch>
-        <Route path="/crypto" component={Crypto} />
-        <Route path="/marketplace" component={MarketPlace} />
+        <Route />
+        <Route />
       </Switch>
-    </div>
+    </MainContainer>
   );
 }
 
--- src/components/MarketPlace/MarketPlaceElements.jsx
+++ src/components/MarketPlace/MarketPlaceElements.jsx
@@ -1,3 +1,9 @@
 import styled from "styled-components";
 
+export const MainContainer = styled.div`
+  width: 80%;
+  margin: auto;
+  border: 1px solid #fff;
+  height: 80vh;
+`;
 export const Menu = styled.div``;
