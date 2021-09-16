27fe1a670429eb4148eb7f672d514a637cf4cd6c
flavio <flavioandrade0201@gmail.com>
2021-09-16 19:27:18 +0300

    Buy nft function created

--- src/components/MarketPlace/MarketPlace.jsx
+++ src/components/MarketPlace/MarketPlace.jsx
@@ -54,6 +54,28 @@ function MarketPlace() {
   if (loadingState === "loaded" && !nfts.length)
     return <h4>No items in marketplace</h4>;
 
+  async function buyNft(nft) {
+    const web3modal = new Web3Modal();
+    const connection = await web3modal.connect();
+    const provider = new ethers.providers.Web3Modal(connection);
+
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(nftmarketaddress, Market.abi, signer);
+
+    const price = ethers.utils.parseUnits(nft.price.toString(), "ether");
+
+    const transaction = await contract.createMarketSale(
+      nftaddress,
+      nft.tokenId,
+      {
+        value: price,
+      }
+    );
+
+    await transaction.wait();
+    loadNfts();
+  }
+
   return (
     <MainContainer>
       <Menu>
