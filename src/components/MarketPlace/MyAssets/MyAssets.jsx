8eeea29c63f2bb9da37c945d5321705f425c6f50
flavio <flavioandrade0201@gmail.com>
2021-09-19 13:47:30 +0300

    MyAssets component imports

--- src/components/MarketPlace/MyAssets/MyAssets.jsx
+++ src/components/MarketPlace/MyAssets/MyAssets.jsx
@@ -1,4 +1,13 @@
 import React from "react";
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Web3Modal from "web3modal";
+
+import { nftmarketaddress, nftaddress } from "../config";
+
+import Market from "../CreatorDashboard/artifacts/contracts/NFTMarket.sol/NFTMarket.json";
+import NFT from "../CreatorDashboard/artifacts/contracts/NFT.sol/NFT.json";
 
 function MyAssets() {
   return (
